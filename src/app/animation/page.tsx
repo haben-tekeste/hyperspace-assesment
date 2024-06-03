cd2134f2e63263a83c1300aa162434f28ed74da2
haben-tekeste <haben.t00@gmail.com>
2024-06-03 15:58:00 +0400

    Disable ssr for canvas rendering

--- src/app/animation/page.tsx
+++ src/app/animation/page.tsx
@@ -1,6 +1,9 @@
 import dynamic from 'next/dynamic'
 
-const Scene = dynamic(() => import('@/components/Scene'), { ssr: true })
+const Scene = dynamic(() => import('@/components/Scene'), {
+  loading: () => <h1> Loading </h1>,
+  ssr: false,
+})
 
 function Home() {
   return (
